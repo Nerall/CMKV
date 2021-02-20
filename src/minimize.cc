e4244ca9fa5d1d341eb4a7f1bfcd8a1acc5e362c
Sillycium <sillycium@DESKTOP-IA9NR08.localdomain>
2021-02-20 21:30:21 +0100

    [MAIN] Changed from random to linear

--- src/minimize.cc
+++ src/minimize.cc
@@ -19,7 +19,7 @@ namespace cmkv
     {
         int sum = 0;
         for (auto j = y - 1; j <= y + 1; j++) {
-            for (auto i = x - 1; i <= x + 1; i++) {
+            for (auto i = x - 1; i != x + 1; i++) {
                 if (i == 0 and j == 0)
                     continue;
                 if (img.has(i, j))
@@ -32,7 +32,7 @@ namespace cmkv
 
     image<rgb8_t> minimize(const image<std::uint8_t>& img)
     {
-        auto T = img.width * img.height * 6;
+        //auto T = img.width * img.height * 6;
 
         auto output = convert_binary(img, kThreshold);
         for (std::size_t y = 0; y < img.height; ++y) {
@@ -46,19 +46,21 @@ namespace cmkv
         std::uniform_int_distribution<int> dist_int(0, img.width * img.height - 1);
         std::uniform_real_distribution<> dist_float(0, 1);
 
-        for (auto t = 0U; t < T; t++) {
-            auto rand_int = dist_int(generator);
-            int x = rand_int % img.width;
-            int y = rand_int / img.width;
-            auto delta = -2. * kGamma * (img(x, y) - kThreshold) * (output(x, y) - kThreshold)
-                         -2. * kBeta * (output(x, y) - kThreshold) * sum_neighbours(output, x, y);
+        for (std::size_t y = 0; y < img.height; y++)
+            for (std::size_t x = 0; x < img.width; x++)
+            {
+                //auto rand_int = dist_int(generator);
+                //int x = rand_int % img.width;
+                //int y = rand_int / img.width;
+                auto delta = -2. * kGamma * (img(x, y) - kThreshold) * (output(x, y) - kThreshold)
+                             -2. * kBeta * (output(x, y) - kThreshold) * sum_neighbours(output, x, y);
 
-            auto r = dist_float(generator);
-            if (log(r) < delta)
-                output(x, y) = 0;
-            else
-                output(x, y) = 255;
-        }
+                auto r = dist_float(generator);
+                if (log(r) < delta)
+                    output(x, y) = 0;
+                else
+                    output(x, y) = 255;
+            }
 
         return convert_rgb(output);
     }
